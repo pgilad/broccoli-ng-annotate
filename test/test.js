13a45512d0a8fab91577af1af6fd90ff2f987695
Gilad Peleg <giladp007@gmail.com>
2014-05-25 23:08:13 +0300

    change test dirs

--- test/test.js
+++ test/test.js
@@ -5,14 +5,14 @@ var fs = require('fs');
 var rimraf = require('rimraf');
 
 describe('broccoli-ng-annotate', function() {
-	afterEach(function() {
-		rimraf.sync('temp');
+	afterEach(function(done) {
+		rimraf('temp', done);
 	});
 
 	it('should annotate file', function() {
 		assert.equal(
 			fs.readFileSync('temp/fixture.js', 'utf8').trim(),
-			fs.readFileSync('fixture/expected.js', 'utf8').trim()
+			fs.readFileSync('test/expected/expected.js', 'utf8').trim()
 		);
 	});
 });
